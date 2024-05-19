ef53bccd47dbcfa255d57c305ec6db3fa4fc6d4e
DinhNhatHai <dinhnhathai1@gmail.com>
2024-05-19 14:05:59 +0700

    cap nhat

--- src/main/java/com/EquipMgmt/EquipMgmtspringboot/Models/Category.java
+++ src/main/java/com/EquipMgmt/EquipMgmtspringboot/Models/Category.java
@@ -1,7 +1,6 @@
 package com.EquipMgmt.EquipMgmtspringboot.Models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
